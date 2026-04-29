d40be5bc045c5f2ccf78acd30d0e2b12d87f3fe3
Ceina Ellison <ellisoce@classwork.engr.oregonstate.edu>
2026-04-29 11:14:49 -0700

    corrected PK location

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,5 @@
+-- Group 2: Eman Alturky, Ceina Ellison
+
 SET FOREIGN_KEY_CHECKS = 0;
 SET AUTOCOMMIT = 0;
 
@@ -8,38 +10,42 @@ DROP TABLE IF EXISTS Trainers;
 DROP TABLE IF EXISTS Equipment_Records;
 
 CREATE TABLE Members(
-    member_id INT AUTO_INCREMENT PRIMARY KEY,
+    member_id INT AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone_number VARCHAR(15),
     membership_start_date DATE NOT NULL
     trainer_id INT NULL,
+    PRIMARY KEY (member_id)
     FOREIGN KEY (trainer_id) REFERENCES Trainers(trainer_id) ON DELETE SET NULL
 );
 
 CREATE TABLE Trainers(
-    trainer_id INT AUTO_INCREMENT PRIMARY KEY,
+    trainer_id INT AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     specialty VARCHAR(100) NULL,
-    hourly_rate DECIMAL(19,2) NOT NULL
+    hourly_rate DECIMAL(19,2) NOT NULL,
+    PRIMARY KEY (trainer_id)
 );
 
 CREATE TABLE Classes(
-    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    class_id INT AUTO_INCREMENT,
     class_name VARCHAR(100) NOT NULL,
     description TEXT NULL,
     max_capacity INT NOT NULL,
     trainer_id INT NOT NULL,
     room_location VARCHAR(50) NOT NULL,
+    PRIMARY KEY (class_id),
     FOREIGN KEY (trainer_id) REFERENCES Trainers(trainer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Equipment_Records(
-    equipment_id INT AUTO_INCREMENT PRIMARY KEY,
+    equipment_id INT AUTO_INCREMENT,
     item_name VARCHAR(100) NOT NULL,
     maintenance_status VARCHAR(50) NOT NULL,
     purchase_date DATE NULL,
-    location VARCHAR(50) NOT NULL
+    location VARCHAR(50) NOT NULL,
+    PRIMARY KEY (equipment_id)
 );
