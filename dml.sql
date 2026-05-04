f6e2c4b8da4c124b9699903b807252d8f83cedf5
Eman Alturky <alturkye@oregonstate.edu>
2026-05-04 09:51:12 -0700

    added update + delete queries for members + trainers

--- dml.sql
+++ dml.sql
@@ -11,6 +11,19 @@ SELECT * FROM Members;
 INSERT INTO Members (first_name, last_name, email, phone_number, membership_start_date, trainer_id)
 VALUES (:first_name_input, :last_name_input, :email_input, :phone_input, :start_date_input, :trainer_id_from_dropdown);
 
+-- UPDATE: modify a member's information based on the ID selected from the table
+UPDATE Members
+SET first_name = :first_name_input,
+    last_name = :last_name_input,
+    email = :email_input,
+    phone_number = :phone_input,
+    membership_start_date = :start_date_input,
+    trainer_id = :trainer_id_from_dropdown
+WHERE member_id = :member_id_selected_from_table;
+
+-- DELETE: remove a member from the database
+DELETE FROM Members WHERE member_id = :member_id_selected_from_table;
+
 ------
 -- TRAINERS PAGE
 ------
@@ -19,4 +32,15 @@ SELECT * FROM Trainers;
 
 -- INSERT: add a new trainer
 INSERT INTO Trainers (first_name, last_name, specialization, hourly_rate)
-VALUES (:first_name_input, :last_name_input, :specialization, :hourly_rate_input);
+VALUES (:first_name_input, :last_name_input, :specialization_input, :hourly_rate_input);
+
+-- UPDATE: edit trainer details
+UPDATE Trainers
+SET first_name = :first_name_input,
+    last_name = :last_name_input,
+    specialization = :specialization_input,
+    hourly_rate = :hourly_rate_input
+WHERE trainer_id = :trainer_id_selected_from_table;
+
+-- DELETE: remove a trainer
+DELETE FROM Trainers WHERE trainer_id = :trainer_id_selected_from_table;
